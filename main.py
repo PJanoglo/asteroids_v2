979f9bff746adee25b50ec90d9654c19b979c6f2
PJanoglo <paul.janoglo@posteo.de>
2025-05-11 13:52:57 +0200

    delta time created

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import pygame
 from constants import *
 
 def main():
-    # initializing pygame
     pygame.init()
-
-    # creates the GUI-windows for the game
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +15,8 @@ def main():
             
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 
